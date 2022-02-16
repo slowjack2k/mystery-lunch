1afb86e239a02f5b86fb5de6de139cbd15916dd4
Dieter Späth <shad0wrunner@gmx.de>
2022-02-16 06:18:13 +0100

    Configured chromeium in order to work with docker.

--- spec/support/chrome.rb
+++ spec/support/chrome.rb
@@ -1,9 +1,19 @@
 RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    if ENV["SHOW_BROWSER"] == "true"
-      driven_by :selenium_chrome
+  config.before(:each, type: :system) do |example|
+    if example.metadata[:js]
+      if ENV["SHOW_BROWSER"] == "true"
+        driven_by :selenium_chrome do |option|
+          option.add_argument("no-sandbox")
+          option.add_argument("disable-gpu")
+        end
+      else
+        driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400] do |option|
+          option.add_argument("no-sandbox")
+          option.add_argument("disable-gpu")
+        end
+      end
     else
-      driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+      driven_by(:rack_test)
     end
   end
 end
