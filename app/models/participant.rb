b26374338fa6a7aea374bc70290a66228e4c7467
Dieter Späth <shad0wrunner@gmx.de>
2022-02-20 14:31:52 +0100

    Added a method to get all peers of a participation

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,8 @@
 class Participant < ApplicationRecord
   belongs_to :employee
   belongs_to :lunch
+
+  def peers
+    self.class.where(lunch: lunch, lunch_group: lunch_group).where.not(employee: employee).all
+  end
 end
