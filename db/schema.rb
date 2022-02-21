b84548778804bfa10f37e97dfdfef6e01e227627
Dieter Späth <shad0wrunner@gmx.de>
2022-02-21 11:44:44 +0100

    Added column email to employees.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_19_090723) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_21_104242) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_19_090723) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "email"
     t.index ["deleted_at"], name: "index_employees_on_deleted_at"
   end
 
